9cfc17a3f77607d1e8ecc05fa3794c05539ec782
Everett Toews <everett@apache.org>
2014-09-10 15:21:40 -0500

    Switch to environ

--- project.clj
+++ project.clj
@@ -4,8 +4,9 @@
   :license {:name "Apache License, Version 2.0"
             :url "http://www.apache.org/licenses/LICENSE-2.0.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [log4j "1.2.13"]
                  [org.slf4j/slf4j-log4j12 "1.6.4"]
+                 [log4j "1.2.13"]
+                 [environ "1.0.0"]
                  [twitter-api "0.7.6"]]
   :main ^:skip-aot tweet.core
   :target-path "target/%s"
--- src/tweet/core.clj
+++ src/tweet/core.clj
@@ -1,30 +1,19 @@
 (ns tweet.core
   (:gen-class)
   (:use [clojure.pprint :only (pprint)])
+  (:use [environ.core :only (env)])
   (:require
     [clojure.edn :as edn]
     [twitter.oauth :refer :all]
     [twitter.api.restful :refer :all]))
 
-(defn load-twitter-config
-  "Load and return your Twitter configuration.
-   Copy twitter.edn.template to twitter.edn and edit."
-  ([] (load-twitter-config "resources/twitter.edn"))
-  ([filename]
-  (try
-    (edn/read-string (slurp filename))
-    (catch java.io.FileNotFoundException e
-      (throw (java.io.FileNotFoundException.
-              (str (.getMessage e) " Copy twitter.edn.template to twitter.edn and edit.")))))))
-
 (defn -main
-  "I don't do a whole lot ... yet."
+  "Small app to tweet a status with Clojure."
   [& args]
-  (def twitter-config (load-twitter-config))
   (def twitter-creds
-    (make-oauth-creds (:app-consumer-key twitter-config)
-                      (:app-consumer-secret twitter-config)
-                      (:user-access-token twitter-config)
-                      (:user-access-token-secret twitter-config)))
+    (make-oauth-creds (env :twitter-app-consumer-key)
+                      (env :twitter-app-consumer-secret)
+                      (env :twitter-user-access-token)
+                      (env :twitter-user-access-token-secret)))
   (statuses-update :oauth-creds twitter-creds
                    :params {:status "Tweeting with #clojure from twitter-api"}))
