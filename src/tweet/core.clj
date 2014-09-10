7958eb38065140795bc3542736ff5d5ccf6b4e79
Everett Toews <everett@apache.org>
2014-09-10 16:48:37 -0500

    Param for status

--- src/tweet/core.clj
+++ src/tweet/core.clj
@@ -3,17 +3,21 @@
   (:use [clojure.pprint :only (pprint)])
   (:use [environ.core :only (env)])
   (:require
-    [clojure.edn :as edn]
+    [clojure.string :as string]
+    [clojure.tools.cli :refer [parse-opts]]
     [twitter.oauth :refer :all]
     [twitter.api.restful :refer :all]))
 
 (defn -main
-  "Small app to tweet a status with Clojure."
+  "Small app to tweet a status with Clojure.
+  Usage: twitter-status-clj STATUS
+  Example: twitter-status-clj \"Tweeting with Clojure\""
   [& args]
+  (def status (nth args 0))
   (def twitter-creds
     (make-oauth-creds (env :twitter-app-consumer-key)
                       (env :twitter-app-consumer-secret)
                       (env :twitter-user-access-token)
                       (env :twitter-user-access-token-secret)))
   (statuses-update :oauth-creds twitter-creds
-                   :params {:status "Tweeting with #clojure from twitter-api"}))
+                   :params {:status status}))
